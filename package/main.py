f6c0bb48cc6578d558e684333828e7c30305b112
RobertMRR <robert.mr729@gmail.com>
2025-02-18 22:13:55 +0100

    player m

--- package/main.py
+++ package/main.py
@@ -1,1 +1,41 @@
-print("hellooo!")
+import pygame
+from player import Player
+
+pygame.init()
+screen = pygame.display.set_mode((1280,720))
+clock = pygame.time.Clock()
+running = True
+dt = 0
+
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() /2)
+
+new_player = Player()
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill("purple")
+
+    pygame.draw.circle(screen, "red", player_pos, 40)
+    
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        player_pos.y -= 300 * dt
+        new_player.walk()
+    if keys[pygame.K_s]:
+        player_pos.y += 300 * dt
+        new_player.walk()
+    if keys[pygame.K_a]:
+        player_pos.x -= 300 * dt
+        new_player.walk()
+    if keys[pygame.K_d]:
+        player_pos.x += 300 * dt
+        new_player.walk()
+
+
+    pygame.display.flip()
+    dt = clock.tick(60) / 1000
+
+pygame.quit()
---
+++ package/player.py
@@ -0,0 +1,11 @@
+class Player():
+
+    """siema to ja a to moje klocki"""
+    def __init__(self):
+        pass
+
+    def walk(self):
+        print("bro is walking hehehe")
+
+if __name__ == '__main__':
+    print("hello its me")
